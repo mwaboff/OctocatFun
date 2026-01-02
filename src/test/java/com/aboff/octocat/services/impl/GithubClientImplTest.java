33cd4468f3d9ae0fa54573f0ecdb8d07a55e1403
Michael Aboff <mwaboff@gmail.com>
2026-01-01 23:01:03 -0500

    Updated test

--- src/test/java/com/aboff/octocat/services/impl/GithubClientImplTest.java
+++ src/test/java/com/aboff/octocat/services/impl/GithubClientImplTest.java
@@ -29,26 +29,12 @@ class GithubClientImplTest {
 
     @Test
     void testGetUserDetails() throws IOException {
-        // Since 'github' field is package-private/private, we can use
-        // ReflectionTestUtils or if it was autowired, InjectMocks helps.
-        // However, the original code lazily initializes it. We want to inject the mock.
-        // InjectMocks should place 'github' mock into 'githubClient'.
-
-        // Wait, the original code initializes github if null.
-        // "if (this.github == null) { this.github = GitHub.connectAnonymously(); }"
-        // So if we inject a mock into 'this.github', it won't be null, and it will use
-        // the mock. Good.
 
         String username = "octocat";
 
         when(github.getUser(username)).thenReturn(ghUser);
         when(ghUser.getLogin()).thenReturn(username);
         when(ghUser.getPublicRepoCount()).thenReturn(0);
-        // If we don't mock getRepositories(), buildFromUser might fail if it iterates.
-        // buildFromUser mocks. Let's look at buildFromUser again.
-        // It calls GithubRepositoryDto.buildListFromUser(userResponse)
-        // which iterates userResponse.getRepositories().
-        // So we need to mock that too.
 
         // Mocking an empty map for repositories
         doReturn(java.util.Collections.emptyMap()).when(ghUser).getRepositories();
